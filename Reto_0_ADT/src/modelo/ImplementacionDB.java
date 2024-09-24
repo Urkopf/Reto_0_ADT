c6c3c0087338f0cf3cc873c7cf8cf36541c0f094
2dam <2dam@HZ375759.TartangaLH.eus>
2024-09-24 10:23:37 +0200

    Correccion

--- Reto_0_ADT/src/modelo/ImplementacionDB.java
+++ Reto_0_ADT/src/modelo/ImplementacionDB.java
@@ -676,7 +676,7 @@ public class ImplementacionDB implements IDao {
 
             anadirAFichero(convocatoria);
         }
-
+    }
 
     @Override
     public void agregarUnidadEnunciado(Integer idenunciado, Integer idunidad) {
